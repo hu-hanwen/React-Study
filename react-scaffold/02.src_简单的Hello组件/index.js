0e860cf0e4df0eb52ba1d4ad3a5068ac84c6100c
Alan_Hu <hu_hanwen@163.com>
2025-03-17 23:17:27 +0800

    todoList案例静态页面搭建

--- react-scaffold/02.src_简单的Hello组件/index.js
+++ react-scaffold/02.src_简单的Hello组件/index.js
@@ -1,11 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+/** 该文件是入口文件  */
 
+// 引入React
+import React from "react";
+// 引入ReactDOM
+import ReactDOM from "react-dom/client";
+
+// 引入App根组件
+import App from "./App";
+// 新版本的创建方式
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// 渲染App组件
 root.render(
 	<React.StrictMode>
-		<App />
+		<App></App>
 	</React.StrictMode>,
 );

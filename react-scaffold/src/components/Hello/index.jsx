246c4fa590a2753fd43b67afe62d2c7cdc1e0e32
Alan_Hu <hu_hanwen@163.com>
2025-03-17 20:36:41 +0800

    一个简单的Hello组件

---
+++ react-scaffold/src/components/Hello/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./index.css";
+
+export default class Hello extends Component {
+	render() {
+		return (
+			<div className="hello">
+				<h2>我是Hello组件</h2>
+			</div>
+		);
+	}
+}
--- react-scaffold/src/index.js
+++ react-scaffold/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/** 该文件是入口文件  */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 引入React
+import React from "react";
+// 引入ReactDOM
+import ReactDOM from "react-dom/client";
+
+// 引入App根组件
+import App from "./App";
+// 新版本的创建方式
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// 渲染App组件
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App></App>
+	</React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

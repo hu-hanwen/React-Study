e6efb0008d2050d3e2eae3ca60cbee772f4afa3d
Alan_Hu <hu_hanwen@163.com>
2025-03-18 22:27:06 +0800

    配置代理服务器

--- react-scaffold/src/App.jsx
+++ react-scaffold/src/App.jsx
@@ -1,88 +1,9 @@
 import React, { Component } from "react";
-import Header from "./components/Header";
-import List from "./components/List";
-import Footer from "./components/Footer";
-
-import "./App.css";
-
 export default class App extends Component {
-	// 初始化数据
-	state = {
-		taskList: [
-			{ id: "001", taskName: "学习React", isDone: false },
-			{ id: "002", taskName: "学习微信小程序", isDone: false },
-			{ id: "003", taskName: "学习Vue", isDone: true },
-		],
-	};
-
-	// 添加新任务
-	addTask = newTaskItem => {
-		const { taskList } = this.state;
-		this.setState({
-			taskList: [newTaskItem, ...taskList],
-		});
-	};
-
-	// 更新任务
-	updateTaskItem = (currentId, isChecked) => {
-		const { taskList } = this.state;
-		taskList.forEach(taskItem => {
-			if (taskItem.id === currentId) {
-				taskItem.isDone = isChecked;
-			}
-		});
-		this.setState({
-			taskList,
-		});
-	};
-
-	// 删除任务
-	removeTaskItem = currentId => {
-		const { taskList } = this.state;
-		const newTaskList = taskList.filter(taskItem => {
-			return taskItem.id !== currentId;
-		});
-		this.setState({
-			taskList: newTaskList,
-		});
-	};
-
-	// 全选/全不选
-	handlerIsAllCheck = isChecked => {
-		const { taskList } = this.state;
-		const newTaskList = taskList.map(taskItem => {
-			return { ...taskItem, isDone: isChecked };
-		});
-		this.setState({
-			taskList: newTaskList,
-		});
-	};
-
-	// 清除已完成任务
-	clearDoneTask = () => {
-		const { taskList } = this.state;
-		const newTaskList = taskList.filter(taskItem => {
-			return !taskItem.isDone;
-		});
-		this.setState({
-			taskList: newTaskList,
-		});
-	};
-
 	render() {
-		const { taskList } = this.state;
-
 		return (
-			<div className="todo-container">
-				<div className="todo-wrap">
-					<Header addTask={this.addTask}></Header>
-					<List taskList={taskList} updateTaskItem={this.updateTaskItem} removeTaskItem={this.removeTaskItem}></List>
-					<Footer
-						taskList={taskList}
-						handlerIsAllCheck={this.handlerIsAllCheck}
-						clearDoneTask={this.clearDoneTask}
-					></Footer>
-				</div>
+			<div>
+				<h2>我是App组件</h2>
 			</div>
 		);
 	}

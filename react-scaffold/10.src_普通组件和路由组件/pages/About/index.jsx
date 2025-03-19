3f3469ec9e97ee70263c9e7c9506d2658435f721
Alan_Hu <hu_hanwen@163.com>
2025-03-19 14:56:58 +0800

    普通组件和路由组件

--- react-scaffold/10.src_普通组件和路由组件/pages/About/index.jsx
+++ react-scaffold/10.src_普通组件和路由组件/pages/About/index.jsx
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class About extends Component {
 	render() {
-		return <div>我是About组件的内容</div>;
+		return <h2>我是About组件的内容</h2>;
 	}
 }
--- react-scaffold/10.src_普通组件和路由组件/pages/Home/index.jsx
+++ react-scaffold/10.src_普通组件和路由组件/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class Home extends Component {
 	render() {
-		return <div>我是Home组件的内容</div>;
+		// console.log("Home组件中收到的props", this.props);
+		return <h2>我是Home组件的内容</h2>;
 	}
 }

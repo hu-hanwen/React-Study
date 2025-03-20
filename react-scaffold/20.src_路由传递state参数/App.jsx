bbec0de7909d39fdc37eb12a0e935df91f70724d
Alan_Hu <hu_hanwen@163.com>
2025-03-20 15:49:48 +0800

    路由传递state参数

---
+++ react-scaffold/20.src_路由传递state参数/App.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import MyNavLink from "./components/MyNavLink";
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<Header></Header>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+							{/* 原生实现 */}
+							{/* <a className="list-group-item" href="./about.html">
+								About
+							</a>
+							<a className="list-group-item active" href="./home.html">
+								Home
+							</a> */}
+
+							{/* react中实现 */}
+							{/* <NavLink className="list-group-item" activeClassName="active" to="/home">
+								Home
+							</NavLink>
+							<NavLink className="list-group-item" activeClassName="active" to="/about">
+								About
+							</NavLink> */}
+
+							{/* 使用二次封装的NavLink */}
+							<MyNavLink to="/home">Home</MyNavLink>
+							<MyNavLink to="/about">About</MyNavLink>
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								{/* 注册路由 */}
+								<Switch>
+									{/* Switch内置组件作用：只匹配一个路由 */}
+									<Route path="/home" component={Home}></Route>
+									<Route path="/about" component={About}></Route>
+									{/* redirect重定向： 匹配不到路由时，重定向到指定路由 */}
+									<Redirect to="/home"></Redirect>
+								</Switch>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}

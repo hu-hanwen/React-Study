22521373b0f66e3bb48a67b6c3ceef0cb2972a21
Alan_Hu <hu_hanwen@163.com>
2025-03-23 00:22:29 +0800

    备份各组件完成数据共享

---
+++ react-scaffold/33.src_各组件完成数据共享/redux/reducers/person.js
@@ -0,0 +1,15 @@
+import { ADD_PERSON } from "../constant";
+
+const initState = [];
+const personReducer = (preState = initState, action) => {
+	const { type, data } = action;
+	switch (type) {
+		case ADD_PERSON:
+			return [data, ...preState];
+
+		default:
+			return preState;
+	}
+};
+
+export default personReducer;

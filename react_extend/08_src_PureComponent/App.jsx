777d9d09b2d9f5b0607ce4b73d53cba0c41cb107
Alan_Hu <hu_hanwen@163.com>
2025-03-25 16:32:25 +0800

    react拓展-使用React.PureComponent代替React.Component

---
+++ react_extend/08_src_PureComponent/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import A from "./components/Demo";
+
+export default class App extends Component {
+	render() {
+		return (
+			<>
+				<A></A>
+			</>
+		);
+	}
+}

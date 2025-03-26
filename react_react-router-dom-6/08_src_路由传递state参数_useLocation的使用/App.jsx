4c91ef965cf0f58710fc9966ac807de988d9a466
Alan_Hu <hu_hanwen@163.com>
2025-03-26 12:31:07 +0800

    路由传递state参数-useLocation的使用

---
+++ react_react-router-dom-6/08_src_路由传递state参数_useLocation的使用/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { NavLink, useRoutes } from "react-router-dom";
+import routes from "./routes";
+
+export default function App() {
+	const element = useRoutes(routes);
+
+	return (
+		<>
+			<div className="row">
+				<div className="col-xs-offset-2 col-xs-8">
+					<div className="page-header">
+						<h2>React Router Demo</h2>
+					</div>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col-xs-2 col-xs-offset-2">
+					<div className="list-group">
+						<NavLink className="list-group-item" to="/about">
+							About
+						</NavLink>
+						<NavLink className="list-group-item" to="/home">
+							Home
+						</NavLink>
+					</div>
+				</div>
+				<div className="col-xs-6">
+					<div className="panel">
+						<div className="panel-body">
+							{/* <Routes>
+								<Route path="/about" element={<About></About>}></Route>
+								<Route path="/home" element={<Home></Home>}></Route>
+								<Route path="/" element={<Navigate to="/about" replace></Navigate>}></Route>
+							</Routes> */}
+							{element}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
